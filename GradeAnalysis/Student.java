ad52e56ad59ae6dfbc192bdc8d969ae59b28fe03
ShannonL788 <160933684+ShannonL788@users.noreply.github.com>
2025-09-18 10:27:51 +0100

    Create Student.java

---
+++ GradeAnalysis/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    String id;
+    double finalScore, labExam, examPercent, labPercent;
+
+    public Student(String id, double finalScore, double labExam, double examPercent, double labPercent) {
+        this.id = id;
+        this.finalScore = finalScore;
+        this.labExam = labExam;
+        this.examPercent = examPercent;
+        this.labPercent = labPercent;
+    }
+}
+
+
